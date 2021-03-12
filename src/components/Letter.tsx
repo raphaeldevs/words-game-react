58f740c34a4329d190fac20586e2b665179a904f
Raphael Corrêa <raphaeldeoliveiracorrea@gmail.com>
2021-03-12 18:32:29 -0300

    add dynamic animation delay

--- src/components/Letter.tsx
+++ src/components/Letter.tsx
@@ -4,17 +4,24 @@ interface LetterProps {
   letter: string
   selectedClassName: string
   onClickEvent?: boolean
+  animationDelay?: string
 }
 
-export function Letter({ letter, selectedClassName, onClickEvent }: LetterProps) {
+export function Letter({
+  letter,
+  selectedClassName,
+  onClickEvent,
+  animationDelay
+}: LetterProps) {
   const [isSelected, setIsSelected] = useState(false)
 
   return (
     <span
+      style={{ animationDelay }}
       className={isSelected ? selectedClassName : ''}
       onClick={() => onClickEvent && setIsSelected(!isSelected)}
     >
-      { letter }
+      {letter}
     </span>
   )
 }
