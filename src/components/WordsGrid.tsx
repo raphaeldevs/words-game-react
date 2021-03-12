d8c6c979a7022bcb4e820cfc5b2f51b6e20489a0
Raphael Corrêa <raphaeldeoliveiracorrea@gmail.com>
2021-03-12 03:26:01 -0300

    render sequence letters

--- src/components/WordsGrid.tsx
+++ src/components/WordsGrid.tsx
@@ -1,22 +1,26 @@
+import { useEffect, useState } from 'react'
+import styles from '../styles/components/WordsGrid.module.scss'
+import { getLettersSequence } from '../utils/words'
+
+import { Letter } from './Letter'
+
 export function WordsGrid() {
+  const [lettersSequence, setLettersSequence] = useState<string[]>([])
+
+  useEffect(() => {
+    setLettersSequence(getLettersSequence('DAVID'))
+  }, [])
+
   return (
-    <main>
-      <span>d</span>
-      <span>d</span>
-      <span>d</span>
-      <span>d</span>
-      <span>d</span>
-      <span>d</span>
-      <span>d</span>
-      <span>d</span>
-      <span>d</span>
-      <span>d</span>
-      <span>d</span>
-      <span>d</span>
-      <span>d</span>
-      <span>d</span>
-      <span>d</span>
-      <span>d</span>
+    <main className={styles.wordsGrid}>
+      {lettersSequence.map(letter => (
+        <Letter
+          key={Math.random()}
+          letter={letter}
+          selectedClassName={styles.selected}
+          onClickEvent
+        />
+      ))}
     </main>
   )
 }
